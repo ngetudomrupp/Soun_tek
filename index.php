6e8fe7988dc6b262666bd4c173ce0dad8e68b5cf
ngetudom <ngetudom9999@gmail.com>
2024-07-01 15:06:25 +0700

    no change

--- index.php
+++ index.php
@@ -1,29 +1,28 @@
 <!Doctype html>
 <?php
-include("actions/db.php");
+include_once("actions/db.php");
 $db = new Database();
 if (isset($_POST['id_nbr']) && isset($_POST['code_nbr'])) {
   $con = "cus_products.nbr_id='" . $_POST['id_nbr'] . "' AND cus_products.code='" . $_POST['code_nbr'] . "'";
   $result = $db->getProductByCode($con);
 }
-$base_dir = $db->base_dir();
-include($base_dir . "/views/header.php");
+include_once("views/header.php");
 ?>
 
 <body>
   <?php
-  include($base_dir . "/views/navbar.php");
+  include_once("views/navbar.php");
   ?>
   <!-- Sections:Start -->
   <div data-bs-spy="scroll" class="scrollspy-example">
     <!-- Hero: Start -->
     <?php
-    include($base_dir . "/views/hero_section.php");
+    include_once("views/hero_section.php");
     ?>
     <!-- Hero: End -->
     <!-- Results: Start -->
     <?php
-    include($base_dir . "/views/product_detail.php");
+    include_once("views/product_detail.php");
     ?>
     <!-- Results Us: End -->
   </div>
@@ -31,12 +30,12 @@ include($base_dir . "/views/header.php");
 
   <!-- Footer: Start -->
   <?php
-  include($base_dir . "/views/footer.php");
+  include_once("views/footer.php");
   ?>
   <!-- Footer: End -->
   <!-- Main JS -->
-  <script src="<?= $db->base_url(); ?>/assets/vendor/js/front-main.js"></script>
-  <script src="<?= $db->base_url(); ?>/assets/vendor/js/bootstrap.js"></script>
+  <script src="<?= $db->url(); ?>/assets/vendor/js/front-main.js"></script>
+  <script src="<?= $db->url(); ?>/assets/vendor/js/bootstrap.js"></script>
 </body>
 
 </html>
